f7f0bd12d1fd9b96cc816f87297787fc62fd1b45
Arael D. Espinosa Pérez <40150522+cl8dep@users.noreply.github.com>
2021-04-24 14:01:18 -0400

    Deploy to godaddy

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cd /home/streaming/apps/events-backend
+sudo git pull https://github.com/Streaming-Cuba/events-backend.git
+dotnet publish --configuration Release
+sudo systemctl restart kestrel-helloapp.service
