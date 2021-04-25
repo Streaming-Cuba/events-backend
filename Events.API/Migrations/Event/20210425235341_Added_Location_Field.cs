c389cd76a572bf1a1b057bc9c88bf8be40336f10
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-04-25 19:55:12 -0400

    Added migration for updated model

---
+++ Events.API/Migrations/Event/20210425235341_Added_Location_Field.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Events.API.Migrations.Event
+{
+    public partial class Added_Location_Field : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Events",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Events");
+        }
+    }
+}
