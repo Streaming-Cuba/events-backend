728f1bc4f36f9d3fe697d8bb82b9b3152c9f9271
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-05-14 11:59:08 -0400

    Added entry to enumerate entries in a folder

--- Events.API/Controllers/MediaController.cs
+++ Events.API/Controllers/MediaController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Events.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -69,6 +70,7 @@ namespace Events.API.Controllers
         [HttpPost("file/{path}")]
         public async Task<IActionResult> CreateFile([FromRoute] string path, [FromQuery] bool overwrite, [Required] IFormFile file)
         {
+            // FIX: Added filter to file from request
             path = Uri.UnescapeDataString(path);
             if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
                 return BadRequest(new
@@ -104,5 +106,34 @@ namespace Events.API.Controllers
         [HttpGet("file/{path}")]
         public IActionResult DownloadFile([FromRoute] string path) 
                 => RedirectPermanent($"https://media.streamingcuba.com/{Uri.UnescapeDataString(path)}");
+
+        [HttpGet("folder/{path}")]
+        public IActionResult EnumerateEntries([FromRoute] string path) 
+        {
+            path = Uri.UnescapeDataString(path);
+            if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
+                return BadRequest(new
+                {
+                    error = "The path contains invalid characters"
+                });
+
+            string directoryPath = Path.Join(_basePath, path);
+            if (!Directory.Exists(directoryPath))
+                return BadRequest(new
+                {
+                    error = "This folder not exists"
+                });
+            
+            var directoryInfo = new DirectoryInfo(directoryPath);            
+            List<EntryInfo> result = directoryInfo.EnumerateFileSystemInfos().Select(x => new EntryInfo
+            {
+                Name = x.Name,
+                Extension = x.Extension,
+                ModificationTime = x.LastWriteTimeUtc,
+                IsDir = x.Attributes.HasFlag(FileAttributes.Directory)
+            }).ToList();
+
+            return Ok(result);
+        }
     }
 }
