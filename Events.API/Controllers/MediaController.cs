ffb84d0b416d43b7b169be43f220ffaf5b157066
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-05-14 11:27:39 -0400

    Addded endpoint to upload media files

--- Events.API/Controllers/MediaController.cs
+++ Events.API/Controllers/MediaController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -16,21 +16,22 @@ namespace Events.API.Controllers
     {
         private readonly string _basePath;
 
-        public MediaController (IConfiguration configuration)
+        public MediaController(IConfiguration configuration)
         {
             _basePath = configuration["Media:Path"];
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpPost("folder/{path}")]
         public IActionResult CreateFolder([FromRoute] string path)
         {
-            path = Uri.UnescapeDataString(path);            
+            path = Uri.UnescapeDataString(path);
             if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
                 return BadRequest(new
                 {
                     error = "The path contains invalid characters"
                 });
-            
+
             string directoryPath = Path.Join(_basePath, path);
             if (Directory.Exists(directoryPath))
                 return BadRequest(new
@@ -41,16 +42,17 @@ namespace Events.API.Controllers
             return Ok();
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpDelete("folder/{path}")]
         public IActionResult DeleteFolder([FromRoute] string path)
         {
-            path = Uri.UnescapeDataString(path);            
+            path = Uri.UnescapeDataString(path);
             if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
                 return BadRequest(new
                 {
                     error = "The path contains invalid characters"
                 });
-            
+
             string directoryPath = Path.Join(_basePath, path);
             if (!Directory.Exists(directoryPath))
                 return BadRequest(new
@@ -59,6 +61,41 @@ namespace Events.API.Controllers
                 });
 
             Directory.Delete(directoryPath, true);
+            return Ok();
+        }
+
+        [HttpPost("file/{path}")]
+        public async Task<IActionResult> CreateFile([FromRoute] string path, [FromQuery] bool overwrite, IFormFile file)
+        {
+            path = Uri.UnescapeDataString(path);
+            if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
+                return BadRequest(new
+                {
+                    error = "The path contains invalid characters"
+                });
+
+            string filePath = Path.Join(_basePath, path);
+            if (!overwrite && System.IO.File.Exists(filePath))
+                return BadRequest(new
+                {
+                    error = "This file already exists"
+                });
+
+            try
+            {
+                using (var stream = System.IO.File.OpenWrite(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+            catch (PathTooLongException)
+            {
+                return BadRequest(new 
+                {
+                    error = "The result path is too long"
+                });
+            }
+
             return Ok();
         }
     }
