1c763ba48792ccd9df9ea438f032812babe1334c
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-05-15 11:23:47 -0400

    Fix media route

--- Events.API/Controllers/MediaController.cs
+++ Events.API/Controllers/MediaController.cs
@@ -24,10 +24,9 @@ namespace Events.API.Controllers
         }
 
         [Authorize(Roles = "Administrator")]
-        [HttpPost("folder/{path}")]
+        [HttpPost("folder/{*path}")]
         public IActionResult CreateFolder([FromRoute] string path)
         {
-            path = Uri.UnescapeDataString(path);
             if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
                 return BadRequest(new
                 {
@@ -45,10 +44,9 @@ namespace Events.API.Controllers
         }
 
         [Authorize(Roles = "Administrator")]
-        [HttpDelete("folder/{path}")]
+        [HttpDelete("folder/{*path}")]
         public IActionResult DeleteFolder([FromRoute] string path)
         {
-            path = Uri.UnescapeDataString(path);
             if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
                 return BadRequest(new
                 {
@@ -67,11 +65,10 @@ namespace Events.API.Controllers
         }
 
         [Authorize(Roles = "Administrator")]
-        [HttpPost("file/{path}")]
+        [HttpPost("file/{*path}")]
         public async Task<IActionResult> CreateFile([FromRoute] string path, [FromQuery] bool overwrite, [Required] IFormFile file)
         {
             // FIX: Added filter to file from request
-            path = Uri.UnescapeDataString(path);
             if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
                 return BadRequest(new
                 {
@@ -103,27 +100,31 @@ namespace Events.API.Controllers
             return Ok();
         }
 
-        [HttpGet("file/{path}")]
+        [HttpGet("file/{*path}")]
         public IActionResult DownloadFile([FromRoute] string path) 
-                => RedirectPermanent($"https://media.streamingcuba.com/{Uri.UnescapeDataString(path)}");
+                => RedirectPermanent($"https://media.streamingcuba.com/{path}");
 
         [Authorize(Roles = "Administrator")]
-        [HttpGet("folder/{path?}")]
+        [HttpGet("folder/{*path}")]
         public ActionResult<List<EntryInfo>> EnumerateEntries([FromRoute] string path) 
         {
-            path = Uri.UnescapeDataString(path);
-            if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
-                return BadRequest(new
-                {
-                    error = "The path contains invalid characters"
-                });
+            string directoryPath = null;
+            if (path != null){
+                if (Path.GetInvalidPathChars().Any(x => path.Contains(x)))
+                    return BadRequest(new
+                    {
+                        error = "The path contains invalid characters"
+                    });
 
-            string directoryPath = Path.Join(_basePath, path);
-            if (!Directory.Exists(directoryPath))
-                return BadRequest(new
-                {
-                    error = "This folder not exists"
-                });
+                directoryPath = Path.Join(_basePath, path);
+                if (!Directory.Exists(directoryPath))
+                    return BadRequest(new
+                    {
+                        error = "This folder not exists"
+                    });
+            } else {
+                directoryPath = _basePath;
+            }
             
             var directoryInfo = new DirectoryInfo(directoryPath);            
             List<EntryInfo> result = directoryInfo.EnumerateFileSystemInfos().Select(x => new EntryInfo
