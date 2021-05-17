87c69a4ead8644e1d8b380a3584561b49774a896
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-05-16 23:59:28 -0400

    Added migration to new changes

--- Events.API/Migrations/Event/20210517035802_Added_New_References.Designer.cs
+++ Events.API/Migrations/Event/20210517035802_Added_New_References.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Events.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Events.API.Migrations.Event
 {
     [DbContext(typeof(EventContext))]
-    partial class EventContextModelSnapshot : ModelSnapshot
+    [Migration("20210517035802_Added_New_References")]
+    partial class Added_New_References
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
