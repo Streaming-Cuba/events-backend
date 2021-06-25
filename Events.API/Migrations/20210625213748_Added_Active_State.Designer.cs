ece94d988376fc25c829cbf92c4ce491c26e1071
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-06-25 17:38:41 -0400

    Added migrations for active state in accounts

--- Events.API/Migrations/20210625213748_Added_Active_State.Designer.cs
+++ Events.API/Migrations/20210625213748_Added_Active_State.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Events.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Events.API.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20210625213748_Added_Active_State")]
+    partial class Added_Active_State
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Events.API/Migrations/20210625213748_Added_Active_State.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Events.API.Migrations
+{
+    public partial class Added_Active_State : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Roles",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Roles",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AccountRoles",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "AccountRoles",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AccountRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "AccountRoles");
+        }
+    }
+}
